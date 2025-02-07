130319e5a959ee7c064fb1cf01f76f1e411d3d05
Reza Pishva <rpishva@gmail.com>
2025-02-07 13:25:46 +0330

    first commit

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ html_table = f"""
     <td style="border: 1px solid black; padding: 8px;{get_color(dbscan)}">{dbscan}</td>
   </tr>
   <tr>
-    <td style="border: 1px solid black; padding: 8px;">DBSCAN Clustering</td>
+    <td style="border: 1px solid black; padding: 8px;">Isolation forest Clustering</td>
     <td style="border: 1px solid black; padding: 8px;{get_color(iso)}">{iso}</td>
   </tr>
 </table>
