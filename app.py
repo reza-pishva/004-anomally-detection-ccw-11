9b495e80f0394906cef083aeb16f62c7cfc367cd
Reza Pishva <rpishva@gmail.com>
2025-02-07 14:11:14 +0330

    first commit

--- app.py
+++ app.py
@@ -227,6 +227,14 @@ def get_color(value):
 html_table = f"""
 <table style="width:100%; border-collapse: collapse;">
   <tr>
+    <td style="border: 1px solid black; padding: 8px;">Isolation forest Clustering</td>
+    <td style="border: 1px solid black; padding: 8px;{get_color(iso)}">{iso}</td>
+  </tr>
+  <tr>
+    <td style="border: 1px solid black; padding: 8px;">OPTICS</td>
+    <td style="border: 1px solid black; padding: 8px;{get_color(optics)}">{optics}</td>
+  </tr>
+  <tr>
     <th style="border: 1px solid black; padding: 8px;">Clustering algorithm</th>
     <th style="border: 1px solid black; padding: 8px;">Result</th>
   </tr>
@@ -254,14 +262,7 @@ html_table = f"""
     <td style="border: 1px solid black; padding: 8px;">DBSCAN Clustering</td>
     <td style="border: 1px solid black; padding: 8px;{get_color(dbscan)}">{dbscan}</td>
   </tr>
-  <tr>
-    <td style="border: 1px solid black; padding: 8px;">Isolation forest Clustering</td>
-    <td style="border: 1px solid black; padding: 8px;{get_color(iso)}">{iso}</td>
-  </tr>
-  <tr>
-    <td style="border: 1px solid black; padding: 8px;">OPTICS</td>
-    <td style="border: 1px solid black; padding: 8px;{get_color(optics)}">{optics}</td>
-  </tr>
+  
 </table>
 """
 # Use st.markdown to display the HTML table
